3b089bbe8b12efd394a56f689cd94dde0fa9c698
Filipe Esch <filipe.esch@farfetch.com>
2023-03-31 11:10:23 +0100

    start project

---
+++ TaskMaestro/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+public interface ITaskHandler<in TInput, TOutput>
+{
+    Task<TOutput> HandleAsync(TInput input, CancellationToken cancellationToken);
+}
+
+public interface IAsyncTaskHandler<in TInput, TOutput>
+{
+    Task<TOutput> HandleAsync(TInput input, CancellationToken cancellationToken);
+}
+
+public interface ITask
+{
+    string Id { get; }
+
+    void WaitFor(string taskId);
+}
+
+public interface ITaskManager
+{
+    Task StartAsync(string taskId);
+}
